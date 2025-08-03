1cab48fd94705c54dee4632ada370a96ee8f0cc6
Peter Sharpe <peterdsharpe@gmail.com>
2025-08-03 17:31:51 -0400

    Add reports demo

---
+++ examples/make_report/make_reports.py
@@ -0,0 +1,22 @@
+from memorylane import profile
+import torch
+from rich.console import Console
+from pathlib import Path
+
+console = Console(force_jupyter=False, width=1000, record=True)  # type: ignore
+
+
+@profile(_console=console)
+def my_function():
+    x = torch.randn(5120, 5120, device="cuda")
+    x = x @ x
+    x = x.relu()
+    x = x.mean()
+    return x
+
+my_function()
+
+folder = Path(__file__).parent
+
+console.save_html(folder / "memorylane_report.html", clear=False)
+console.save_text(folder / "memorylane_report.txt", clear=False)
